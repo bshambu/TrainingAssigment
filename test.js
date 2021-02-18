97d35f83f73ac10b26ba77f500dccbdaa59181be
bshambu <45159837+bshambu@users.noreply.github.com>
2021-02-18 18:17:32 +0530

    javascript assignments added
    javascript assignments added

---
+++ test.js
@@ -0,0 +1,11 @@
+  
+        function printOdd(i) {
+            if(i % 2 !== 0) {
+              console.log(i);
+            }else{
+                console.log("this is even");
+            }
+        }
+        printOdd(3);
+        printOdd(4);
+        printOdd(7);
